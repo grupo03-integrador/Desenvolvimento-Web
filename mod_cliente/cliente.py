15a8710d4ed0358e1faec5e18fca94b8ab118534
Renata Lia <renatalia@uniplaclages.edu.br>
2020-11-30 22:51:23 -0300

    Alterações no projeto integrador

--- mod_cliente/cliente.py
+++ mod_cliente/cliente.py
@@ -1,79 +1,82 @@
 #coding: utf-8
-from flask import Blueprint, render_template, request, redirect
-from mod_cliente.clienteDB import Clientes
+from flask import Blueprint, render_template, request, redirect, session, url_for, jsonify
 from mod_login.login import validaSessao
+from mod_cliente.clienteDB import Clientes
 
-bp_cliente = Blueprint('cliente', __name__, url_prefix="/cliente", template_folder='templates')
+bp_cliente = Blueprint('cliente', __name__, url_prefix='/', template_folder='templates')
 
-# FormListaClientes 
-@bp_cliente.route("/", methods=['GET', 'POST'])
-#@validaSessao
-def formListaClientes():
-    cliente = Clientes()
-    res = cliente.selectALL()
-    return render_template('formListaClientes.html', result=res, content_type='application/json')
+@bp_cliente.route("/index", methods=['GET', 'POST'])
+@validaSessao
+def index():
+         cliente=Clientes()
+         res = cliente.selectALL()
+         return render_template('formListaClientes.html', result=res, content_type='application/json')
 
-# FormCadastroCliente
-@bp_cliente.route("/formCadastroCliente", methods=['POST'])
-#@validaSessao
-def formCadastroCliente():
-    cliente=Clientes()
-    return render_template('formCadastroCliente.html', cliente=cliente, content_type='application/json')
-
-#-----------------------------------------------------------------------------------------------------------------------
+@bp_cliente.route("/formCliente", methods=['POST'])
+@validaSessao
+def formCliente():
+         cliente=Clientes()
+         return render_template('formCadastroCliente.html', cliente=cliente, content_type='application/json')
 
 @bp_cliente.route("/formEditCliente", methods=['POST'])
 @validaSessao
 def formEditCliente():
-    cliente=Clientes()
-    cliente.id_cliente = request.form['id_cliente']
-    cliente.selectONE( )
-    return render_template('formCliente.html', cliente=cliente, content_type='application/json')
+        cliente=Clientes()
+        cliente.id_cliente = request.form['id_cliente']
+        cliente.selectONE( )
+        return render_template('formCadastroCliente.html', cliente=cliente, content_type='application/json')
 
 @bp_cliente.route('/addCliente', methods=['POST'])
 @validaSessao
 def addCliente():
-    cliente=Clientes()
-    cliente.id_cliente = request.form['id_cliente']
-    cliente.nome = request.form['nome']
-    cliente.endereco = request.form['endereco']
-    cliente.numero = request.form['numero']
-    cliente.observacao = request.form['observacao']
-    cliente.cep = request.form['cep']
-    cliente.bairro = request.form['bairro']
-    cliente.cidade = request.form['cidade']
-    cliente.estado = request.form['estado']
-    cliente.telefone = request.form['telefone']
-    cliente.email = request.form['email']
-    cliente.login = request.form['login']
-    cliente.senha = request.form['senha']
-    cliente.grupo = request.form['grupo']
-        
-    cliente.insert()
-    return redirect('/cliente/')
+        cliente=Clientes()
+        cliente.id_cliente = request.form['id_cliente']
+        cliente.nome = request.form['nome']
+        cliente.cpf = request.form['cpf']
+        cliente.data_pagamento = request.form['data_pagamento']               
+        cliente.telefone = request.form['telefone']
+        cliente.senha = request.form['senha']
+        cliente.compra_fiado = request.form['compra_fiado']        
+        cliente.insert()
+        return redirect(url_for('cliente.index'))
+
 
 @bp_cliente.route('/editCliente', methods=['POST'])
 @validaSessao
 def editCliente():
-    cliente=Clientes()
-    cliente.id_cliente = request.form['id_cliente']
-    cliente.nome = request.form['nome']
-    cliente.endereco = request.form['endereco']
-    cliente.numero = request.form['numero']
-    cliente.observacao = request.form['observacao']
-    cliente.cep = request.form['cep']
-    cliente.bairro = request.form['bairro']
-    cliente.cidade = request.form['cidade']
-    cliente.estado = request.form['estado']
-    cliente.telefone = request.form['telefone']
-    cliente.email = request.form['email']
-    cliente.login = request.form['login']
-    cliente.senha = request.form['senha']
-    cliente.grupo = request.form['grupo']
+        cliente=Clientes()
+        cliente.id_cliente = request.form['id_cliente']
+        cliente.nome = request.form['nome']
+        cliente.cpf = request.form['cpf']
+        cliente.data_pagamento = request.form['data_pagamento']                
+        cliente.telefone = request.form['telefone']
+        cliente.senha = request.form['senha']
+        cliente.compra_fiado = request.form['compra_fiado']
         
-    if 'salvaEditaUsuarioDB' in request.form:
-        cliente.update()
-    elif 'salvaExcluiUsuarioDB' in request.form:
-        cliente.delete()
+        if 'salvaEditaUsuarioDB' in request.form:
+            cliente.update()
+        elif 'salvaExcluiUsuarioDB' in request.form:   
+            cliente.delete()
+
+        return redirect(url_for('cliente.index'))
         
-    return redirect('/cliente/')
+@bp_cliente.route("/clienteLista")
+@validaSessao
+def listaCliente():
+    return render_template('formListaClientes.html')
+
+
+@bp_cliente.route('/verificaSeLoginExiste', methods = ['POST'])
+@validaSessao
+def verificaSeLoginExiste():
+        cliente = Clientes()
+        cliente.login = request.form['login']
+        try:
+                result = cliente.verificaSeLoginExiste()
+                #Verifica se achou o login no banco
+                if len(result) > 0:
+                          return jsonify(login_existe = True)
+                else:
+                         return jsonify(login_existe = False)
+        except Exception as e:
+                return jsonify(erro = True, mensagem_exception = str(e))
--- mod_funcionario/funcionario.py
+++ mod_funcionario/funcionario.py
@@ -1,24 +1,86 @@
 #coding: utf-8
-from flask import Blueprint, render_template, request, redirect
+from flask import Blueprint, render_template, request, redirect, session, url_for, jsonify
+from mod_login.login import validaSessao
 from mod_funcionario.funcionarioDB import Funcionario
-#from mod_login.login import validaSessao
 
-bp_funcionario = Blueprint('funcionario', __name__, url_prefix="/funcionario", template_folder='templates')
+bp_funcionario = Blueprint('funcionario', __name__, url_prefix='/', template_folder='templates')
 
-# FormListaFuncionario
-@bp_funcionario.route("/", methods=['GET', 'POST'])
-#@validaSessao
-def formListaFuncionarios():
-    funcionario = Funcionario()
-    res = funcionario.selectALL()
-    return render_template('formListaFuncionarios.html', result=res, content_type='application/json')
+@bp_funcionario.route("/indexFunc", methods=['GET', 'POST'])
+@validaSessao
+def indexFunc():
+         funcionario=Funcionario()
+         res = funcionario.selectALL()
+         return render_template('formListaFuncionarios.html', result=res, content_type='application/json')
 
-# FormCadastroFuncionario
-@bp_funcionario.route("/formCadastroFuncionario", methods=['POST'])
-#@validaSessao
-def formCadastroFuncionario():
-    #cliente=Clientes()
-    return render_template('formCadastroFuncionario.html', content_type='application/json')
+@bp_funcionario.route("/formFuncionario", methods=['POST'])
+@validaSessao
+def formFuncionario():
+         funcionario=Funcionario()
+         return render_template('formCadastroFuncionario.html', funcionario=funcionario, content_type='application/json')
 
-#-----------------------------------------------------------------------------------------------------------------------
+@bp_funcionario.route("/formEditFuncionario", methods=['POST'])
+@validaSessao
+def formEditFuncionario():
+        funcionario=Funcionario()
+        funcionario.id_funcionario = request.form['id_funcionario']
+        funcionario.selectONE( )
+        return render_template('formCadastroFuncionario.html', funcionario=funcionario, content_type='application/json')
 
+@bp_funcionario.route('/addFuncionario', methods=['POST'])
+@validaSessao
+def addFuncionario():
+        funcionario=Funcionario()
+        funcionario.id_funcionario = request.form['id_funcionario']        
+        funcionario.matricula = request.form['matricula']
+        funcionario.grupo = request.form['grupo']
+        funcionario.nome = request.form['nome']         
+        funcionario.matricula = request.form['cpf']              
+        funcionario.telefone = request.form['telefone']
+        funcionario.senha = request.form['senha']
+        funcionario.compra_fiado = request.form['compra_fiado'] 
+        funcionario.data_pagamento = request.form['data_pagamento']       
+        funcionario.insert()
+        return redirect(url_for('funcionario.indexFunc'))
+
+
+@bp_funcionario.route('/editFuncionario', methods=['POST'])
+@validaSessao
+def editFuncionario():
+        funcionario=Funcionario()
+        funcionario.id_funcionario = request.form['id_funcionario']        
+        funcionario.matricula = request.form['matricula']
+        funcionario.grupo = request.form['grupo']
+        funcionario.nome = request.form['nome']         
+        funcionario.matricula = request.form['cpf']              
+        funcionario.telefone = request.form['telefone']
+        funcionario.senha = request.form['senha']
+        funcionario.compra_fiado = request.form['compra_fiado'] 
+        funcionario.data_pagamento = request.form['data_pagamento']
+        
+        if 'salvaEditaFuncionarioDB' in request.form:
+            funcionario.update()
+        elif 'salvaExcluiFuncionariooDB' in request.form:   
+            funcionario.delete()
+
+        return redirect(url_for('funcionario.indexFunc'))
+        
+@bp_funcionario.route("/funcionarioLista")
+@validaSessao
+def listaFuncionario():
+    return render_template('formListaFuncionarios.html')
+
+
+@bp_funcionario.route('/verificaSeLoginExiste', methods = ['POST'])
+@validaSessao
+def verificaSeLoginExiste():
+        funcionario = Funcionario       ()
+        funcionario.login = request.form['login']
+        try:
+                result = cliente.verificaSeLoginExiste()
+                #Verifica se achou o login no banco
+                if len(result) > 0:
+                          return jsonify(login_existe = True)
+                else:
+                         return jsonify(login_existe = False)
+        except Exception as e:
+                return jsonify(erro = True, mensagem_exception = str(e))
